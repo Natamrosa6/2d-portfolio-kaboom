99140afda5b3cc786b8642eb4b19e1577fd8aa35
Natalia Tamara  Rosa <109100346+Natamrosa6@users.noreply.github.com>
2024-08-08 16:13:54 +0200

    cambio bed test

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const dialogueData = {
     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
     <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
 
-    bed: `Está sin hacer... Es la única cosa del mundo que no soporto...`,
+    bed: `Es la única cosa del mundo que no soporto...`,
 
     resume: `subir mi CV <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
     poner mi mail`,
